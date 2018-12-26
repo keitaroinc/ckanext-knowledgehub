b503f5c2dfc67d56971cc1712c954593996e6948
Vladimir Tomanovski <vladoohr@gmail.com>
2018-12-26 10:55:49 +0100

    Add data model for analysis framework

---
+++ ckanext/knowledgehub/model/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""This package contains knowledfehub's data models
+"""
+
+from ckanext.knowledgehub.model.analysis_framework import (AnalysisFramework,
+                                                           af_db_setup)
+
+
+__all__ = (
+    'AnalysisFramework',
+    'af_db_setup'
+)
