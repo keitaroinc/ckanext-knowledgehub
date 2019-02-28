ce0fa47af3b22aed22c75f4b5465d613fe909bf9
Andreyka <andrea.bozarevska@keitaro.com>
2019-02-28 14:24:25 +0100

    Extend resource metadata

--- ckanext/knowledgehub/plugin.py
+++ ckanext/knowledgehub/plugin.py
@@ -54,6 +54,7 @@ class KnowledgehubPlugin(plugins.SingletonPlugin, DefaultDatasetForm):
             'get_theme_options': h.get_theme_options,
             'get_sub_theme_options': h.get_sub_theme_options,
             'pg_array_to_py_list': h.pg_array_to_py_list,
+	    'resource_view_get_fields': h.resource_view_get_fields,
         }
 
     # IDatasetForm
@@ -80,6 +81,21 @@ class KnowledgehubPlugin(plugins.SingletonPlugin, DefaultDatasetForm):
             'username': defaults,
             'password': defaults,
             'sql': defaults,
+	        #'some_name':defaults,
+	        #'worker_title': defaults,
+            'unit_supported': defaults,
+            'im_product_name': defaults,
+            'indicator_label': defaults,
+	        'indicator_code':defaults,
+	        'indicator_old_name': defaults,
+            'unit_focal_point': defaults,
+            'sector': defaults,
+            'data_sources': defaults,
+	        'frequency_of_update':defaults,
+	        #'ind_description': defaults,
+	        'sub_themes': defaults,
+            'analytical_value': defaults,
+            'indicator_type': defaults,
         })
 
         return schema
@@ -111,6 +127,21 @@ class KnowledgehubPlugin(plugins.SingletonPlugin, DefaultDatasetForm):
             'username': defaults,
             'password': defaults,
             'sql': defaults,
+	        #'some_name':defaults,
+	        #'worker_title': defaults,
+            'unit_supported': defaults,
+            'im_product_name': defaults,
+            'indicator_label': defaults,
+	        'indicator_code':defaults,
+	        'indicator_old_name': defaults,
+            'unit_focal_point': defaults,
+            'sector': defaults,
+            'data_sources': defaults,
+	        'frequency_of_update':defaults,
+	        #'ind_description': defaults,
+	        'sub_themes': defaults,
+            'analytical_value': defaults,
+            'indicator_type': defaults,
         })
         return schema
 
@@ -123,3 +154,4 @@ class KnowledgehubPlugin(plugins.SingletonPlugin, DefaultDatasetForm):
     # IPackageController
     def before_index(self, data_dict):
         return data_dict
+
