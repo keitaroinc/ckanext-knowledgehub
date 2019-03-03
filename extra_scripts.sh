07d2bdc7c210667823f9ecc21467591c542d15b9
Vladimir Tomanovski <vladoohr@gmail.com>
2019-03-03 18:24:11 +0100

    Print config file of workers

--- extra_scripts.sh
+++ extra_scripts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "[extra_scripts.sh] Starting ckan workers"
+echo "${APP_DIR}/production.ini"
+
 paster --plugin=ckan jobs worker bulk --config="${APP_DIR}/production.ini" > /tmp/worker-bulk.log 2>&1 &
 paster --plugin=ckan jobs worker priority --config="${APP_DIR}/production.ini" > /tmp/worker-priority.log 2>&1 &
 paster --plugin=ckan jobs worker --config="${APP_DIR}/production.ini" > /tmp/worker.log 2>&1 &
