72d60947bd822e80993a4cbd5bf1c38f7408cfd3
Dusko Bogdanovski <dusko.bogdanovski@keitaro.com>
2019-04-05 16:56:31 +0200

    Chart views crud functionality

--- ckanext/knowledgehub/views/chart_view.py
+++ ckanext/knowledgehub/views/chart_view.py
@@ -37,6 +37,80 @@ chart_view = Blueprint(
 )
 
 
+def _process_post_data(data, resource_id):
+
+    config = {}
+    filters = []
+    for k, v in data.items():
+
+        if k.startswith('data_filter_name_'):
+            filter = {}
+            filter_id = k.split('_')[-1]
+            filter['order'] = int(filter_id)
+            filter['name'] = \
+                data.pop('data_filter_name_{}'.format(filter_id))
+            filter['value'] = \
+                data.pop('data_filter_value_{}'.format(filter_id))
+            filters.append(filter)
+
+        config['type'] = \
+            data['chart_field_type']
+        config['title'] = \
+            data['chart_field_title']
+        config['x_axis'] = \
+            data['chart_field_x_axis_column']
+        config['y_axis'] = \
+            data['chart_field_y_axis_column']
+        config['category_name'] = \
+            data['chart_field_category_name']
+        config['color'] = \
+            data['chart_field_color']
+        config['y_label'] = \
+            data['chart_field_y_label']
+        config['x_text_rotate'] = \
+            data['chart_field_x_text_rotate']
+        config['x_text_multiline'] = \
+            data['chart_field_x_text_multiline']
+        config['dynamic_reference_type'] = \
+            data['chart_field_dynamic_reference_type']
+        config['dynamic_reference_label'] = \
+            data['chart_field_dynamic_reference_label']
+        config['dynamic_reference_factor'] = \
+            data['chart_field_dynamic_reference_factor']
+        config['sort'] = \
+            data['chart_field_sort']
+        config['chart_padding_left'] = \
+            data['chart_field_chart_padding_left']
+        config['chart_padding_bottom'] = \
+            data['chart_field_chart_padding_bottom']
+        config['tick_count'] = \
+            data['chart_field_tick_count']
+        config['show_legend'] = \
+            data['chart_field_legend']
+        config['padding_top'] = \
+            data['chart_field_padding_top']
+        config['data_format'] = \
+            data['chart_field_data_format']
+        config['tooltip_name'] = \
+            data['chart_field_tooltip_name']
+        config['show_labels'] = \
+            data['chart_field_labels']
+        config['y_tick_format'] = \
+            data['chart_field_y_ticks_format']
+        config['sql_string'] = \
+            data['sql_string']
+
+    config['filters'] = json.dumps(filters)
+
+    view_dict = {}
+    view_dict['resource_id'] = resource_id
+    view_dict['title'] = config['title']
+    view_dict['view_type'] = 'chart'
+    view_dict.update(config)
+
+    return view_dict
+
+
 @chart_view.before_request
 def before_request():
     try:
@@ -46,6 +120,104 @@ def before_request():
     except NotAuthorized:
         base.abort(403, _(u'Need to be system '
                           u'administrator to administer'))
+
+
+class EditView(MethodView):
+    u''' Edit existing Chart view '''
+
+    def _prepare(self):
+
+        context = {
+            u'model': model,
+            u'session': model.Session,
+            u'user': g.user,
+            u'auth_user_obj': g.userobj
+        }
+        try:
+            pass
+            # check_access(u'edit_indicator_view', context)
+        except NotAuthorized:
+            return base.abort(403, _(u'Unauthorized'
+                                     u' to edit a visualizations'))
+        return context
+
+    def get(self, id, resource_id, view_id, data=None, errors=None,
+            error_summary=None):
+
+        context = self._prepare()
+
+        # update resource should tell us early if the user has privilages.
+        try:
+            check_access('resource_update', context, {'id': resource_id})
+        except NotAuthorized as e:
+            abort(403, _('User %r not authorized to edit %s') % (g.user, id))
+
+        # get resource and package data
+        try:
+            package = get_action('package_show')(context, {'id': id})
+        except (NotFound, NotAuthorized):
+            abort(404, _('Dataset not found'))
+
+        try:
+            resource = get_action('resource_show')(
+                context, {'id': resource_id})
+        except (NotFound, NotAuthorized):
+            abort(404, _('Resource not found'))
+
+        try:
+            old_data = get_action('resource_view_show')(context,
+                                                        {'id': view_id})
+            data = data or old_data
+        except (NotFound, NotAuthorized):
+            abort(404, _('View not found'))
+
+        vars = {
+            'pkg': package,
+            'res': resource,
+            'data': data,
+            'errors': errors,
+            'error_summary': error_summary
+        }
+
+        return base.render(
+            u'view/chart/edit_chart_view_base.html',
+            extra_vars=vars
+        )
+
+    def post(self, id, resource_id, view_id, data=None, errors=None,
+             error_summary=None):
+
+        context = self._prepare()
+
+        try:
+            data = logic.clean_dict(
+                dictization_functions.unflatten(
+                    logic.tuplize_dict(logic.parse_params(request.form))))
+        except dictization_functions.DataError:
+            base.abort(400, _(u'Integrity Error'))
+
+        view_dict = _process_post_data(data, resource_id)
+
+        to_delete = data.pop('delete', None)
+
+        try:
+            if to_delete:
+                view_dict['id'] = view_id
+                get_action('resource_view_delete')(context, view_dict)
+            elif view_id:
+                view_dict['id'] = view_id
+                resource_view = \
+                    get_action('resource_view_update')(context, view_dict)
+        except logic.NotAuthorized:
+            base.abort(403, _(u'Unauthorized to edit resource'))
+        except logic.ValidationError as e:
+            errors = e.error_dict
+            error_summary = e.error_summary
+            return self.get(id, resource_id, view_dict, errors, error_summary)
+
+        return h.redirect_to(controller='package',
+                             action='resource_views',
+                             id=id, resource_id=resource_id)
 
 
 class CreateView(MethodView):
@@ -71,7 +243,6 @@ class CreateView(MethodView):
             error_summary=None):
 
         context = self._prepare()
-        data = data or {}
 
         # update resource should tell us early if the user has privilages.
         try:
@@ -102,7 +273,7 @@ class CreateView(MethodView):
         }
 
         return base.render(
-            u'view/chart/new_chart_form.html',
+            u'view/chart/new_chart_view_base.html',
             extra_vars=vars
         )
 
@@ -118,75 +289,7 @@ class CreateView(MethodView):
         except dictization_functions.DataError:
             base.abort(400, _(u'Integrity Error'))
 
-        config = {}
-        filters = []
-        for k, v in data.items():
-
-            if k.startswith('data_filter_name_'):
-                filter = {}
-                filter_id = k.split('_')[-1]
-                filter['order'] = int(filter_id)
-                filter['name'] = \
-                    data.pop('data_filter_name_{}'.format(filter_id))
-                filter['value'] = \
-                    data.pop('data_filter_value_{}'.format(filter_id))
-                filters.append(filter)
-
-            config['type'] = \
-                data['chart_field_type']
-            config['title'] = \
-                data['chart_field_title']
-            config['x_axis'] = \
-                data['chart_field_x_axis_column']
-            config['y_axis'] = \
-                data['chart_field_y_axis_column']
-            config['category_name'] = \
-                data['chart_field_category_name']
-            config['color'] = \
-                data['chart_field_color']
-            config['y_label'] = \
-                data['chart_field_y_label']
-            config['x_text_rotate'] = \
-                data['chart_field_x_text_rotate']
-            config['x_text_multiline'] = \
-                data['chart_field_x_text_multiline']
-            config['dynamic_reference_type'] = \
-                data['chart_field_dynamic_reference_type']
-            config['dynamic_reference_label'] = \
-                data['chart_field_dynamic_reference_label']
-            config['dynamic_reference_factor'] = \
-                data['chart_field_dynamic_reference_factor']
-            config['sort'] = \
-                data['chart_field_sort']
-            config['chart_padding_left'] = \
-                data['chart_field_chart_padding_left']
-            config['chart_padding_bottom'] = \
-                data['chart_field_chart_padding_bottom']
-            config['tick_count'] = \
-                data['chart_field_tick_count']
-            config['show_legend'] = \
-                data['chart_field_legend']
-            config['padding_top'] = \
-                data['chart_field_padding_top']
-            config['data_format'] = \
-                data['chart_field_data_format']
-            config['tooltip_name'] = \
-                data['chart_field_tooltip_name']
-            config['show_labels'] = \
-                data['chart_field_labels']
-            config['y_tick_format'] = \
-                data['chart_field_y_ticks_format']
-            config['sql_string'] = \
-                data['sql_string']
-
-        config['filters'] = json.dumps(filters)
-        data['config'] = config
-
-        view_dict = {}
-        view_dict['resource_id'] = resource_id
-        view_dict['title'] = config['title']
-        view_dict['view_type'] = 'chart'
-        view_dict.update(config)
+        view_dict = _process_post_data(data, resource_id)
 
         try:
             resource_view = \
@@ -196,7 +299,7 @@ class CreateView(MethodView):
         except logic.ValidationError as e:
             errors = e.error_dict
             error_summary = e.error_summary
-            return self.get(id, resource_id, data, errors, error_summary)
+            return self.get(id, resource_id, view_dict, errors, error_summary)
 
         return h.redirect_to(controller='package',
                              action='resource_views',
@@ -205,3 +308,5 @@ class CreateView(MethodView):
 
 chart_view.add_url_rule(u'/new_chart',
                         view_func=CreateView.as_view(str(u'new_chart')))
+chart_view.add_url_rule(u'/edit_chart/<view_id>',
+                        view_func=EditView.as_view(str(u'edit_chart')))
