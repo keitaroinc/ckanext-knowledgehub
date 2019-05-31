2af118652229a14430556400723d163d27b3886d
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2019-05-31 11:23:48 +0200

    Finish implementation

--- ckanext/knowledgehub/helpers.py
+++ ckanext/knowledgehub/helpers.py
@@ -228,19 +228,9 @@ def get_visualization_size():
     Get available sizes for displaying visualizations: charts, text box
     :return:
     '''
-    options = [{'text': _('Small Rectangle (1x2)'), 'value': 'size-sm'},
-               {'text': _('Small Wide Rectangle (1x6)'),
-                   'value': 'size-sm wide'},
-               {'text': _('Medium Square (2x2)'), 'value': 'size-sm square'},
-               {'text': _('Medium Rectangle (2x3)'), 'value': 'size-lg'},
-               {'text': _('Large Rectangle (2x4)'),
-                   'value': 'size-sm double square'},
-               {'text': _('Extra Large Rectangle (2x6)'), 'value': 'size-xl'},
-               {'text': _('Large Square (4x4)'), 'value': 'size-lg square'},
-               {'text': _('Medium Vertical (4x2)'),
-                   'value': 'size-sm vertical'},
-               {'text': _('Large Vertical (4x3)'),
-                   'value': 'size-lg vertical'}]
+    options = [{'text': _('Small'), 'value': 'small'},
+               {'text': _('Medium'), 'value': 'medium'},
+               {'text': _('Large'), 'value': 'large'}]
     return options
 
 
