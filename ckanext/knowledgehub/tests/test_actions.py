42fa472147366aa0e6ec8d4cfbcc96114dec5598
Vladimir Tomanovski <vladoohr@gmail.com>
2019-06-25 14:44:05 +0200

    Initial implementation of the unit tests

--- ckanext/knowledgehub/tests/test_actions.py
+++ ckanext/knowledgehub/tests/test_actions.py
@@ -1,21 +1,79 @@
 """Tests for actions.py."""
 
-import ckan.tests.factories as factories
-import ckan.tests.helpers as helpers
 import mock
 import nose.tools
+
+from ckan.tests import factories
+from ckan import plugins
+from ckan.tests import helpers
+from ckan.plugins import toolkit
+
+from ckanext.knowledgehub.model.theme import theme_db_setup
+from ckanext.knowledgehub.model.research_question import setup as rq_db_setup
+from ckanext.knowledgehub.model.sub_theme import setup as sub_theme_db_setup
+from ckanext.knowledgehub.model.dashboard import setup as dashboard_db_setup
+from ckanext.knowledgehub.model.rnn_corpus import setup as rnn_corpus_setup
+from ckanext.knowledgehub.model.resource_feedback import (
+    setup as resource_feedback_setup
+)
+from ckanext.knowledgehub.model.kwh_data import (
+    setup as kwh_data_setup
+)
+from ckanext.knowledgehub.logic.action import create as create_actions
 
 assert_equals = nose.tools.assert_equals
 assert_raises = nose.tools.assert_raises
 assert_not_equals = nose.tools.assert_not_equals
 
 
-class TestAnalyticalFrameworkActions(object):
+class ActionsBase(helpers.FunctionalTestBase):
+    def setup(self):
+        # helpers.reset_db()
+        theme_db_setup()
+        sub_theme_db_setup()
+        rq_db_setup()
+        dashboard_db_setup()
+        resource_feedback_setup()
+        kwh_data_setup()
+        rnn_corpus_setup()
 
-    @classmethod
-    def setup_class(cls):
-        helpers.reset_db()
 
-    def test_analytical_framework_delete(self):
+class TestKWHCreateActions(ActionsBase):
 
-        assert_equals('', '')
+    def test_theme_create(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'ignore_auth': True
+        }
+        data_dict = {
+            'name': 'test-name',
+            'title': 'Test title',
+            'description': 'Test description'
+        }
+        theme = create_actions.theme_create(context, data_dict)
+
+        assert_equals(theme.get('name'), 'test-name')
+
+    def test_sub_theme_create(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'ignore_auth': True
+        }
+        data_dict = {
+            'name': 'test-sub-theme',
+            'title': 'Test title',
+            'description': 'Test description'
+        }
+        theme = create_actions.theme_create(context, data_dict)
+
+        data_dict = {
+            'name': 'sub-theme-name',
+            'title': 'Test title',
+            'description': 'Test description',
+            'theme': theme.get('id')
+        }
+        sub_theme = create_actions.sub_theme_create(context, data_dict)
+
+        assert_equals(sub_theme.get('name'), 'sub-theme-name')
