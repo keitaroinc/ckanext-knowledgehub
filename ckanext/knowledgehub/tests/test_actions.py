5ec7e8e06b0c23cc6c8945f967483c8ffaffae33
Vladimir Tomanovski <vladoohr@gmail.com>
2019-06-26 17:56:19 +0200

    Add unit tests for create actions

--- ckanext/knowledgehub/tests/test_actions.py
+++ ckanext/knowledgehub/tests/test_actions.py
@@ -1,5 +1,7 @@
 """Tests for actions.py."""
 
+import os
+import uuid
 import mock
 import nose.tools
 
@@ -7,6 +9,7 @@ from ckan.tests import factories
 from ckan import plugins
 from ckan.tests import helpers
 from ckan.plugins import toolkit
+from ckan import model
 
 from ckanext.knowledgehub.model.theme import theme_db_setup
 from ckanext.knowledgehub.model.research_question import setup as rq_db_setup
@@ -31,6 +34,25 @@ class User(object):
         self.id = id
 
 
+def create_dataset(**kwargs):
+    sysadmin = factories.Sysadmin()
+    user = User(sysadmin.get('id'))
+
+    context = {
+        'auth_user_obj': user,
+        'user': sysadmin.get('name')
+    }
+
+    data_dict = {
+        'name': str(uuid.uuid4()),
+        'title': 'title',
+        'notes': 'notes',
+        'maintainer': 'John'
+    }
+    data_dict.update(kwargs)
+    return toolkit.get_action('package_create')(context, data_dict)
+
+
 class ActionsBase(helpers.FunctionalTestBase):
     def setup(self):
         helpers.reset_db()
@@ -41,6 +63,7 @@ class ActionsBase(helpers.FunctionalTestBase):
         resource_feedback_setup()
         kwh_data_setup()
         rnn_corpus_setup()
+        os.environ["CKAN_INI"] = 'subdir/test.ini'
 
 
 class TestKWHCreateActions(ActionsBase):
@@ -58,7 +81,7 @@ class TestKWHCreateActions(ActionsBase):
         }
         theme = create_actions.theme_create(context, data_dict)
 
-        assert_equals(theme.get('name'), 'theme-name')
+        assert_equals(theme.get('name'), data_dict.get('name'))
 
     def test_sub_theme_create(self):
         user = factories.Sysadmin()
@@ -82,7 +105,7 @@ class TestKWHCreateActions(ActionsBase):
         }
         sub_theme = create_actions.sub_theme_create(context, data_dict)
 
-        assert_equals(sub_theme.get('name'), 'sub-theme-name')
+        assert_equals(sub_theme.get('name'), data_dict.get('name'))
 
     def test_research_question_create(self):
         user = factories.Sysadmin()
@@ -116,4 +139,137 @@ class TestKWHCreateActions(ActionsBase):
         }
         rq = create_actions.research_question_create(context, data_dict)
 
-        assert_equals(rq.get('name'), 'rq-name')
+        assert_equals(rq.get('name'), data_dict.get('name'))
+
+    def test_resource_view_create(self):
+        dataset = create_dataset()
+        resource = factories.Resource(
+            package_id=dataset['id'],
+            url='https://jsonplaceholder.typicode.com/posts'
+        )
+
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'auth_user_obj': User(user.get('id')),
+            'ignore_auth': True,
+            'model': model,
+            'session': model.Session
+        }
+
+        data_dict = {
+            'resource_id': resource.get('id'),
+            'title': 'Visualization title',
+            'description': 'Visualization description',
+            'view_type': 'chart',
+            'config': {
+                "color": "#59a14f",
+                "y_label": "Usage",
+                "show_legend": "Yes"
+            }
+        }
+        rsc_view = create_actions.resource_view_create(context, data_dict)
+
+        assert_equals(rsc_view.get('package_id'), dataset.get('id'))
+
+    def test_dashboard_create(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'auth_user_obj': User(user.get('id')),
+            'ignore_auth': True,
+            'model': model,
+            'session': model.Session
+        }
+
+        data_dict = {
+            'name': 'internal-dashboard',
+            'title': 'Internal Dashboard',
+            'description': 'Dashboard description',
+            'type': 'internal'
+        }
+        dashboard = create_actions.dashboard_create(context, data_dict)
+
+        assert_equals(dashboard.get('name'), data_dict.get('name'))
+
+    def test_resource_feedback(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'auth_user_obj': User(user.get('id')),
+            'ignore_auth': True,
+            'model': model,
+            'session': model.Session
+        }
+
+        dataset = create_dataset()
+        resource = factories.Resource(
+            package_id=dataset['id'],
+            url='https://jsonplaceholder.typicode.com/posts'
+        )
+
+        data_dict = {
+            'type': 'useful',
+            'dataset': dataset.get('id'),
+            'resource': resource.get('id')
+        }
+        rf = create_actions.resource_feedback(context, data_dict)
+
+        assert_equals(rf.get('dataset'), dataset.get('id'))
+        assert_equals(rf.get('resource'), resource.get('id'))
+
+    def test_kwh_data_create(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'auth_user_obj': User(user.get('id')),
+            'ignore_auth': True,
+            'model': model,
+            'session': model.Session
+        }
+
+        data_dict = {
+            'type': 'theme',
+            'content': 'Refugees in Syria'
+        }
+        kwh_data = create_actions.kwh_data_create(context, data_dict)
+
+        assert_equals(kwh_data.get('content'), data_dict.get('content'))
+
+    def test_corpus_create(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'auth_user_obj': User(user.get('id')),
+            'ignore_auth': True,
+            'model': model,
+            'session': model.Session
+        }
+
+        data_dict = {
+            'corpus': 'KHW corpus'
+        }
+        kwh_corpus = create_actions.corpus_create(context, data_dict)
+
+        assert_equals(kwh_corpus.get('corpus'), data_dict.get('corpus'))
+
+    def test_run_command(self):
+        user = factories.Sysadmin()
+        context = {
+            'user': user.get('name'),
+            'auth_user_obj': User(user.get('id')),
+            'ignore_auth': True,
+            'model': model,
+            'session': model.Session
+        }
+
+        data_dict = {
+            'command': 'db init'
+        }
+        cmd = create_actions.run_command(context, data_dict)
+
+        assert_equals(
+            cmd,
+            'Successfully run command: knowledgehub -c %s %s'
+            % (os.environ.get('CKAN_INI'), data_dict.get('command'))
+        )
