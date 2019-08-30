087a45ddb2725453cd82a8fe67b5434afba93577
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-08-30 15:40:32 +0200

    Remove ckan solr config

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -25,7 +25,7 @@ sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 echo "SOLR config..."
 # Solr is multicore for tests on ckan master, but it's easier to run tests on
 # Travis single-core. See https://github.com/ckan/ckan/issues/2972
-sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+#sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
 
 echo "Initialising the database..."
 cd ckan
