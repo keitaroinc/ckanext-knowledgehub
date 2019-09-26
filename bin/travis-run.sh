ec0db57477bb47a9fbbec46820a7e0b17fdd978f
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-26 15:13:01 +0200

    running datapusher

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,6 +3,7 @@
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
+sudo python datapusher/main.py deployment/datapusher_settings.py
 
 nosetests --ckan \
           --nologcapture \
