0745cfa8d2184df9496d626deb1580d2d0862117
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 14:15:17 +0200

    changed solr jetty

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -2,13 +2,13 @@
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
+sudo service jetty8 restart
 # cd ..
 # cd datapusher
 # pip list
 # sudo python datapusher/main.py deployment/datapusher_settings.py &
 
-# cd -
+# cd -  
 # cd ckanext-knowledgehub
 
 nosetests --ckan \
