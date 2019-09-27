7af70c05e52c4b619afa03d452a49a073446cfd3
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 12:45:42 +0200

    add ext req

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -14,8 +14,8 @@ git checkout "ckan-2.8.2"
 python setup.py develop
 # Travis has an issue with older version of psycopg2 (2.4.5)
 sed -i 's/psycopg2==2.4.5/psycopg2==2.7.3.2/' requirements.txt
-pip install -r requirements.txt 
-pip install -r dev-requirements.txt
+pip install -r requirements.txt --allow-all-external
+pip install -r dev-requirements.txt --allow-all-external
 cd -
 
 echo "Creating the PostgreSQL user and database..."
