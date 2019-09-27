b9375f793a600dac4ff70652eaa49d9a1540a730
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 10:24:23 +0200

    changed dir

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -15,5 +15,4 @@ nosetests --ckan \
           --cover-package=ckanext.knowledgehub \
           --cover-inclusive \
           --cover-erase \
-          --cover-tests \
-          -w ckanext/knowledgehub/tests
+          --cover-tests
