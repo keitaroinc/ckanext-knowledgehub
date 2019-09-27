d01d16731e32f2e1b2761d5f81da0f1edd1295df
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 10:09:51 +0200

    changed dir

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,11 +3,10 @@
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
-cd -
+cd ..
 cd datapusher
 sudo python datapusher/main.py deployment/datapusher_settings.py
 cd -
-cd knowledgehub
 
 nosetests --ckan \
           --nologcapture \
