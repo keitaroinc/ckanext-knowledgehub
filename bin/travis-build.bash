2824cbc8664af1621c08edd0a1b709186be74ef6
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 18:43:58 +0200

    try to fix file location again... and again..

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -43,9 +43,9 @@ pip install -r dev-requirements.txt
 echo "Setup database tables for ckanext-knowledgehub"
 knowledgehub -c ckan/test-core.ini db init
 
-# echo "Moving test.ini into a subdir..."
-# mkdir subdir
-# mv test.ini subdir
+echo "Moving test.ini into a subdir..."
+mkdir subdir
+mv test.ini subdir
 
 cd ..
 echo "Installing ckanext-datapusher and it's requirements... "
