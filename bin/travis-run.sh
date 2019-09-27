056a832411d55c3c61b9978148e368b531f2e843
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 15:13:51 +0200

    tryout

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty8
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty8 restart
 # cd ..
