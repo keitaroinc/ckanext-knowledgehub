d95a34194fad04565f76d13a3a57948763f874aa
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 10:31:09 +0200

    changed command

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -5,7 +5,7 @@ sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 cd ..
 cd datapusher
-sudo python datapusher/main.py deployment/datapusher_settings.py
+sudo python datapusher/main.py deployment/datapusher_settings.py &
 cd -
 
 nosetests --ckan \
