c4c3454c200ca69e28b026c644cab20fb38d67dc
Andreyka <andrea.bozarevska@keitaro.com>
2019-09-27 10:34:13 +0200

    list

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -5,6 +5,7 @@ sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 cd ..
 cd datapusher
+pip list
 sudo python datapusher/main.py deployment/datapusher_settings.py &
 cd -
 
