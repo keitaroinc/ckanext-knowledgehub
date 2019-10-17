be6b49d26965e291bebb396ccf824eaef517bd4b
Andreyka <andrea.bozarevska@keitaro.com>
2019-10-17 13:23:58 +0200

    added butt chart

--- ckanext/knowledgehub/fanstatic/javascript/modules/chart.js
+++ ckanext/knowledgehub/fanstatic/javascript/modules/chart.js
@@ -309,6 +309,72 @@ ckan.module('chart', function () {
                         }
                     }
                 }
+                if(this.options.chart_type === 'buttchart') {
+                    var margin = {top: 20, right: 30, bottom: 40, left: 100},
+                    width = 550 - margin.left - margin.right,
+                    height = 200 - margin.top - margin.bottom;
+                
+                  var x = d3.scale.linear()
+                      .range([0, width]);
+                
+                  var y = d3.scale.ordinal()
+                      .rangeRoundBands([0, height], 0.1);
+                
+                  var xAxis = d3.svg.axis()
+                      .scale(x)
+                      .orient("bottom")
+                      .ticks(7)
+                
+                  var yAxis = d3.svg.axis()
+                      .scale(y)
+                      .orient("left")
+                      .tickSize(0)
+                
+                  var svg = d3.select("body").append("svg")
+                      .attr("width", width + margin.left + margin.right)
+                      .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                    console.log("tuka sme")
+                   
+                    console.log("chart selection")
+                    x.domain(d3.extent(data, function(d) { return d.interactions; })).nice();
+                    y.domain(data.map(function(d) { return d.age; }));
+            
+                    var minInteractions = Math.min.apply(Math, data.map(function(o){return o.interactions;}))
+                    yAxis.tickPadding(Math.abs(x(minInteractions) - x(0)) + 10);
+            
+                    var bar = svg.selectAll(".bar")
+                        .data(data)
+            
+                    bar.enter().append("rect")
+                        .attr("class", function(d) { return "bar bar--" + (d.interactions < 0 ? "negative" : "positive"); })
+                        .attr("x", function(d) { return x(Math.min(0, d.interactions)); })
+                        .attr("y", function(d) { return y(d.age); })
+                        .attr("width", function(d) { return Math.abs(x(d.interactions) - x(0)); })
+                        .attr("height", y.rangeBand())
+            
+                    bar.enter().append('text')
+                        .attr("text-anchor", "middle").attr("x", function(d,i) {
+                            return x(Math.min(0, d.interactions)) + (Math.abs(x(d.interactions) - x(0)) / 2);
+                        })
+                        .attr("y", function(d,i) {
+                            return y(d.age) + (y.rangeBand() / 2);
+                        })
+                        .attr("dy", ".35em")
+                        .text(function (d) { return d.interactions; })
+            
+                    svg.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
+            
+                    svg.append("g")
+                        .attr("class", "y axis")
+                        .attr("transform", "translate(" + x(0) + ",0)")
+                        .call(yAxis);
+                }
+
                 if (this.options.chart_type === 'bscatter') {
                     //workaround for bubble charts, scale log base 10 because of large values
                     var rs = d3.scale.log().base(10).domain([1, 1000]).range([0, 10]);
