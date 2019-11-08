57a5a5fb15dcfc1c6f1915f79e39305d2027c048
Andreyka <andrea.bozarevska@keitaro.com>
2019-11-08 13:29:39 +0100

    Add/remove comments

--- ckanext/knowledgehub/controllers/group.py
+++ ckanext/knowledgehub/controllers/group.py
@@ -174,30 +174,3 @@ class KWHGroupController(GroupController):
 
             self._setup_template_variables(context, {'id': id},
                                         group_type=group_type)
-
-
-    # def read(self, id, limit=20):
-    #     group_type = self._ensure_controller_matches_group_type(
-    #         id.split('@')[0])
-
-    #     context = {'model': model, 'session': model.Session,
-    #                'user': c.user,
-    #                'schema': self._db_to_form_schema(group_type=group_type),
-    #                'for_view': True}
-    #     data_dict = {'id': id, 'type': group_type}
-
-    #     # unicode format (decoded from utf8)
-    #     c.q = request.params.get('q', '')
-
-    #     try:
-    #         # Do not query for the group datasets when dictizing, as they will
-    #         # be ignored and get requested on the controller anyway
-    #         data_dict['include_datasets'] = False
-    #         c.group_dict = self._action('group_show')(context, data_dict)
-    #         c.group = context['group']
-    #     except (NotFound, NotAuthorized):
-    #         abort(404, _('Group not found'))
-
-    #     self._read(id, limit, group_type)
-    #     return render(self._read_template(c.group_dict['type']),
-    #                   extra_vars={'group_type': group_type})
