bd1163b4a9b6a94698dda8e5f42fe8b36a58ec2c
Andreyka <andrea.bozarevska@keitaro.com>
2019-11-08 13:25:50 +0100

    Renaming orgs & groups pt2

--- ckanext/knowledgehub/controllers/__init__.py
+++ ckanext/knowledgehub/controllers/__init__.py
@@ -1,5 +1,9 @@
 from ckanext.knowledgehub.controllers.package import KWHPackageController
+from ckanext.knowledgehub.controllers.group import KWHGroupController
+from ckanext.knowledgehub.controllers.organization import KWHOrganizationController
 
 __all__ = [
-    'KWHPackageController'
+    'KWHPackageController',
+    'KWHGroupController',
+    'KWHOrganizationController'
 ]
