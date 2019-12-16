3d3902567a65593f45088bb6c7f59cc4c25e8294
Nikola Lekich <you@example.com>
2019-12-16 15:44:15 +0100

    Changes on the serach.html page for the research questions, now it is highlighted on which tab we are currently on.

--- ckanext/knowledgehub/fanstatic/javascript/modules/render_homepage_results.js
+++ ckanext/knowledgehub/fanstatic/javascript/modules/render_homepage_results.js
@@ -1,16 +1,23 @@
-function showSearchResults(evt, entity) {
-  
-    // hide elements
-    tabcontent = document.getElementsByClassName("tab_content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    var x = document.getElementById(entity);
-            if (x.style.display === "none") {
-                x.style.display = "inline-block";
-            } else {
-                x.style.display = "none";
-            } 
-  
-    document.getElementById(entity).style.display = "inline-block";
+function showSearchResults(evt, entity, id) {
+
+  // hide elements
+  tabcontent = document.getElementsByClassName("tab_content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  var x = document.getElementById(entity);
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+  tabs = document.getElementsByClassName("tab");
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].className = tabs[i].className.replace(" activateTab", "");
+  }
+  evt.currentTarget.className += " activateTab";
+
+
+
+  document.getElementById(entity).style.display = "inline-block";
+}
