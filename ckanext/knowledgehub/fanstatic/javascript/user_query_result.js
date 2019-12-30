d7bd749a7205ea566768bb9d60cfb13a72adbb27
Vladimir Tomanovski <vladoohr@gmail.com>
2019-12-30 16:02:39 +0100

    Add JS module to save User Query Results

--- ckanext/knowledgehub/fanstatic/javascript/user_query_result.js
+++ ckanext/knowledgehub/fanstatic/javascript/user_query_result.js
@@ -2,6 +2,10 @@
     'use strict';
 
     const SKIP_PAGES = ['organization', 'group']
+    const DATASET_TYPE = 'dataset'
+    const RQ_TYPE = 'research_question'
+    const DASHBOARD_TYPE = 'dashboard'
+    const VISUALIZATION_TYPE = 'visualization'
 
     var api = {
         get: function (action, params) {
@@ -19,15 +23,58 @@
         }
     };
 
-    $(document).ready(function () {
-        $('.dataset-list').on('click', '.dataset-heading', function() {
-            var url = window.location.href
-            var parts = url.split('/')
-            var page = parts[3]
+    function saveUserQueryResult(result_type, result_id) {
+        var url = window.location.href
+        var parts = url.split('/')
+        var page = parts[3]
 
-            // only perform this action on home page
-            if (! SKIP_PAGES.includes(page)) {
+        if (!SKIP_PAGES.includes(page)) {
+            var query_text = $('#field-giant-search').val();
+            if (query_text) {
+                var user_id = $('#user-id').val();
+                api.get('user_query_show', {
+                    query_text: query_text,
+                    user_id: user_id
+                })
+                .done(function (data) {
+                    if (data.success) {
+                        var query_id = data.result.id
+                        api.post('user_query_result_create', {
+                            query_id: query_id,
+                            result_type: result_type,
+                            result_id: result_id
+                        })
+                        .done(function (data) {
+                            console.log("User Quere Result: SAVED!");
+                        })
+                        .fail(function (error) {
+                            console.log("user_query_result_create: " + error.statusText);
+                        });
+                    }
+                })
+                .fail(function (error) {
+                    console.log("user_query_show: " + error.statusText);
+                });
             }
+        }
+    }
+
+    $(document).ready(function () {
+        var tab_content = $('.tab_content');
+
+        tab_content.on('click', '.dataset-heading', function() {
+            var result_id = $('#dataset-id').val();
+            saveUserQueryResult(DATASET_TYPE, result_id);
+        });
+
+        tab_content.on('click', '.rq-heading', function () {
+            var result_id = $('#rq-id').val();
+            saveUserQueryResult(RQ_TYPE, result_id);
+        });
+
+        tab_content.on('click', '.dashboard-link', function () {
+            var result_id = $('#dashboard-id').val();
+            saveUserQueryResult(DASHBOARD_TYPE, result_id);
         });
     });
 })(ckan.i18n.ngettext, $);
