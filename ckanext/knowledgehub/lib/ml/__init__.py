2bf30c05545f9dd67419c1a7581a3b5a6330de53
Pavle Jonoski <pavle.jonoski@keitaro.com>
2020-01-02 17:19:24 +0100

    Adds pydoc and fixes for code style.

--- ckanext/knowledgehub/lib/ml/__init__.py
+++ ckanext/knowledgehub/lib/ml/__init__.py
@@ -1,4 +1,9 @@
-from ckanext.knowledgehub.lib.ml.model import NLPProcessor, get_nlp_processor, ModelLocator, model_locator
+from ckanext.knowledgehub.lib.ml.model import (
+    NLPProcessor,
+    get_nlp_processor,
+    ModelLocator,
+    model_locator
+)
 from ckanext.knowledgehub.lib.ml.worker import Worker
 
 
