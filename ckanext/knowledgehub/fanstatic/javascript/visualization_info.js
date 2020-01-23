0083f23839a4ee4f8092c7bbcab8de7a63583609
Nikola Lekich <you@example.com>
2020-01-23 18:10:06 +0100

    new branch for improved accessibility

--- ckanext/knowledgehub/fanstatic/javascript/visualization_info.js
+++ ckanext/knowledgehub/fanstatic/javascript/visualization_info.js
@@ -4,39 +4,38 @@
 "use strict";
 
 
-function make_url(content)
-{
+function make_url(content) {
   var api = {
     get: function (action, params, async) {
-        var api_ver = 3;
-        var base_url = ckan.sandbox().client.endpoint;
-        params = $.param(params);
-        var url = base_url + '/api/' + api_ver + '/action/' + action + '?' + params;
-        if (!async) {
-            $.ajaxSetup({
-                async: false
-            });
-        }
-        return $.getJSON(url);
+      var api_ver = 3;
+      var base_url = ckan.sandbox().client.endpoint;
+      params = $.param(params);
+      var url = base_url + '/api/' + api_ver + '/action/' + action + '?' + params;
+      if (!async) {
+        $.ajaxSetup({
+          async: false
+        });
+      }
+      return $.getJSON(url);
     }
   };
 
-    var url ="";
-    api.get('research_question_show', {
-      'id' : content
-    }, false)
-    .done(function(data) {
+  var url = "";
+  api.get('research_question_show', {
+    'id': content
+  }, false)
+    .done(function (data) {
       api.get('get_rq_url', {
         name: data.result['name']
       }, false).
-      done(function(data) {
-        console.log(window.location.host);
-        var host = window.location.host;
-        url = "http://"+ host + data.result;
-        console.log(url);
-      });
-      });
-      return url;
+        done(function (data) {
+          console.log(window.location.host);
+          var host = window.location.host;
+          url = "http://" + host + data.result;
+          console.log(url);
+        });
+    });
+  return url;
 }
 
 ckan.module('visualization_info', function ($) {
@@ -45,40 +44,49 @@ ckan.module('visualization_info', function ($) {
       var resource_id = this.options.resource_id;
       var table_title = this.options.title;
       var rq_ids = this.options.id;
-      var rqs ="";
-      if(rq_ids == 0)
+      var subtitle = this.options.subtitle
+      var description = this.options.description
+      var rqs = "";
+      var content = "";
+      if (subtitle != "") {
+        content += '<p>' + '<b>Subtitle: </b>' + subtitle + '</p>';
+      }
+      if (description != "") {
+        content += '<p>' + '<b>Description: </b>' + description + '</p>';
+      }
+      if (rq_ids == 0)
         rqs = "This dataset contains no research <br/ > questions.";
       else
         rqs = "<strong> Research Questions: </strong>";
-      var content =  rqs;
+      content = rqs;
       content += '<br/ >';
       var rqs_list = this.options.rqs;
-      if(rqs_list != 0)
-      {
-      
-        rqs_list = rqs_list.replace(/u'/g,"");
-        
-        rqs_list = rqs_list.replace(/'/g,"").replace("[","").replace("]","");
+      if (rqs_list != 0) {
+
+        rqs_list = rqs_list.replace(/u'/g, "");
+
+        rqs_list = rqs_list.replace(/'/g, "").replace("[", "").replace("]", "");
         var remove_comma = rqs_list.split(',');
-        
-        rq_ids = rq_ids.replace(/{/g,"");
-        rq_ids = rq_ids.replace(/}/g,"");
-        rq_ids = rq_ids.replace(/ /g,"");
+
+        rq_ids = rq_ids.replace(/{/g, "");
+        rq_ids = rq_ids.replace(/}/g, "");
+        rq_ids = rq_ids.replace(/ /g, "");
         rq_ids = rq_ids.split(',');
-        for( var i = 0; i < remove_comma.length; ++i)
-        {
-          var x ="";
-          x+= make_url(rq_ids[i]);
-          content += '<a href="' + x + '">"' + remove_comma[i]+ '"</a>';
+        for (var i = 0; i < remove_comma.length; ++i) {
+          var x = "";
+          x += make_url(rq_ids[i]);
+          content += '<a href="' + x + '">"' + remove_comma[i] + '"</a>';
           content += '<br/ >';
         }
       }
-      
-      this.el.popover({title: "Title: "+table_title,
+
+      this.el.popover({
+        title: "Title: " + table_title,
         content: content,
         html: true,
-        placement: 'right'});
+        placement: 'right'
+      });
     }
-    
-    };
+
+  };
 });
