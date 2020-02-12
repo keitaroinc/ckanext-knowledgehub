751447751ed74a47d6e5ab9d4d005987dccfe257
nushkovg <gnuskov@protonmail.com>
2020-02-12 01:18:43 +0100

    Adapted research question indexing to keywords

--- ckanext/knowledgehub/model/research_question.py
+++ ckanext/knowledgehub/model/research_question.py
@@ -52,7 +52,7 @@ research_question = Table(
     Column('state',
            types.UnicodeText,
            default=u'active'),
-    )
+)
 
 
 class ResearchQuestion(DomainObject, Indexed):
@@ -150,31 +150,20 @@ class ResearchQuestion(DomainObject, Indexed):
             data['sub_theme_id'] = sub_theme.id
             data['sub_theme_name'] = sub_theme.name
             data['sub_theme_title'] = sub_theme.title
-        
+
         keywords = set()
         if data.get('tags'):
             for tag in data.get('tags', '').split(','):
-                vocab_id = None
-                vocabs = get_action('vocabulary_list')(
-                    {'ignore_auth': True, }, {})
-                for vocab in vocabs:
-                    tag_list = vocab.get('tags')
-                    for tg in tag_list:
-                        if tg.get('name') == tag:
-                            vocab_id = tg.get('vocabulary_id')
-                            break
-                    if vocab_id:
-                        break
                 tag_obj = get_action('tag_show')(
-                    {'ignore_auth': True, },
-                    {'id': tag, 'vocabulary_id': vocab_id}
+                    {'ignore_auth': True},
+                    {'id': tag}
                 )
-                if tag_obj.get('vocabulary_id'):
-                    vocabulary = get_action('vocabulary_show')(
-                        {'ignore_auth': True, },
-                        {'id': tag_obj.get('vocabulary_id')}
+                if tag_obj.get('keyword_id'):
+                    keyword_obj = get_action('keyword_show')(
+                        {'ignore_auth': True},
+                        {'id': tag_obj.get('keyword_id')}
                     )
-                    keywords.add(vocabulary.get('name'))
+                    keywords.add(keyword_obj.get('name'))
                     if keywords:
                         data['keywords'] = ','.join(keywords)
 
