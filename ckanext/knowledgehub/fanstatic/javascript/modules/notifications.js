3ed802d175b1f4eba8c0e434e5fbbf38e6d7ba14
Pavle Jonoski <pavle.jonoski@keitaro.com>
2020-03-12 15:48:17 +0100

    Implements basic notification system.

--- ckanext/knowledgehub/fanstatic/javascript/modules/notifications.js
+++ ckanext/knowledgehub/fanstatic/javascript/modules/notifications.js
@@ -6,14 +6,22 @@ ckan.module('user-profile', function($){
     $(function(){
         $('.notifications-button').click(function(){
             var notifications = []
+            if ($(this).attr('marked')){
+                return
+            }
+            $(this).attr('marked', true);
             $('.notification-item').each(function(_, itm){
                 console.log('Notification ->', $(itm).attr('id'));
                 notifications.push($(itm).attr('id'))
             })
 
-            if (notifications){
-                $.post('/api/3/action/notifications_read', {
-                    notifications: notifications,
+            if (notifications.length){
+                $.ajax('/api/3/action/notifications_read', {
+                    type: 'POST',
+                    data: JSON.stringify({
+                        notifications: notifications,
+                    }),
+                    dataType: 'json',
                 })
             }
         });
--- ckanext/knowledgehub/model/notification.py
+++ ckanext/knowledgehub/model/notification.py
@@ -3,7 +3,7 @@ from ckan.model.meta import metadata, mapper, Session, engine
 from ckan.model.types import make_uuid
 from ckan.model.domain_object import DomainObject
 
-from sqlalchemy import types, ForeignKey, Column, Table, or_
+from sqlalchemy import types, ForeignKey, Column, Table, or_, update
 from sqlalchemy.sql.expression import func
 import datetime
 import logging
@@ -40,7 +40,7 @@ class Notification(DomainObject):
     def get_notifications(cls, user_id, limit=0, offset=10):
         query = Session.query(cls).filter(
             notification_table.c.recepient == user_id,
-            notification_table.c.seen is not True,
+            notification_table.c.seen != True
         )
         query = query.order_by(notification_table.c.created_at.desc())
 
@@ -52,26 +52,27 @@ class Notification(DomainObject):
     def get_notifications_count(cls, user_id):
         query = Session.query(cls).filter(
             notification_table.c.recepient == user_id,
-            notification_table.c.seen is not True,
+            notification_table.c.seen != True
         )
         return  query.count()
 
     @classmethod
     def mark_read(cls, user_id, notifications=None):
-        statement = Session.query(cls)
+        statement = update(cls)
         
         if notifications:
-            statement = statement.filter(
-                notification_table.c.recepient == user_id,
+            statement = statement.where(
+                notification_table.c.recepient == user_id and \
                 notification_table.c.id.in_(notifications)
             )
         else:
-            statement = statement.filter(
-                notification_table.c.recepient == user_id,
+            statement = statement.where(
+                notification_table.c.recepient == user_id
             )
 
-        statement = statement.update({'seen': True})
+        statement = statement.values({'seen': True})
 
+        Session.execute(statement)
         Session.commit()
 
 
