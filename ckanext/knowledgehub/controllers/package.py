e2945bd30b89bd4c5c8219e5e4fd70c54d5d244a
Vladimir Tomanovski <vladoohr@gmail.com>
2020-03-15 23:24:58 +0100

    Fix string representation of array for shared users on dataset edit page

--- ckanext/knowledgehub/controllers/package.py
+++ ckanext/knowledgehub/controllers/package.py
@@ -640,3 +640,72 @@ class KWHPackageController(PackageController):
 
         return h.redirect_to(controller='package', action='resource_read', id=id,
                              resource_id=resource_id)
+
+    def edit(self, id, data=None, errors=None, error_summary=None):
+        package_type = self._get_package_type(id)
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user, 'auth_user_obj': c.userobj,
+                   'save': 'save' in request.params}
+
+        if context['save'] and not data and request.method == 'POST':
+            return self._save_edit(id, context, package_type=package_type)
+        try:
+            c.pkg_dict = get_action('package_show')(dict(context,
+                                                         for_view=True),
+                                                    {'id': id})
+            context['for_edit'] = True
+            old_data = get_action('package_show')(context, {'id': id})
+            # old data is from the database and data is passed from the
+            # user if there is a validation error. Use users data if there.
+            if data:
+                old_data.update(data)
+            data = old_data
+        except (NotFound, NotAuthorized):
+            abort(404, _('Dataset not found'))
+        # are we doing a multiphase add?
+        if data.get('state', '').startswith('draft'):
+            c.form_action = h.url_for(controller='package', action='new')
+            c.form_style = 'new'
+            return self.new(data=data, errors=errors,
+                            error_summary=error_summary)
+
+        c.pkg = context.get("package")
+        c.resources_json = h.json.dumps(data.get('resources', []))
+
+        try:
+            check_access('package_update', context)
+        except NotAuthorized:
+            abort(403, _('User %r not authorized to edit %s') % (c.user, id))
+        # convert tags if not supplied in data
+        if data and not data.get('tag_string'):
+            data['tag_string'] = ', '.join(h.dict_list_reduce(
+                c.pkg_dict.get('tags', {}), 'name'))
+        errors = errors or {}
+        form_snippet = self._package_form(package_type=package_type)
+        form_vars = {'data': data, 'errors': errors,
+                     'error_summary': error_summary, 'action': 'edit',
+                     'dataset_type': package_type,
+                     }
+        c.errors_json = h.json.dumps(errors)
+
+        shared_with_users = form_vars.get('data').get('shared_with_users')
+        if shared_with_users:
+            if shared_with_users.startswith('{') and \
+               shared_with_users.endswith('}'):
+                shared_with_users = shared_with_users[1:-1]
+            form_vars['data']['shared_with_users'] = \
+                shared_with_users.split(',')
+
+        self._setup_template_variables(context, {'id': id},
+                                       package_type=package_type)
+
+        # we have already completed stage 1
+        form_vars['stage'] = ['active']
+        if data.get('state', '').startswith('draft'):
+            form_vars['stage'] = ['active', 'complete']
+
+        edit_template = self._edit_template(package_type)
+        return render(edit_template,
+                      extra_vars={'form_vars': form_vars,
+                                  'form_snippet': form_snippet,
+                                  'dataset_type': package_type})
