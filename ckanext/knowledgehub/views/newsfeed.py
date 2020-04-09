cc8c0f75358d4826557f4989df4f05699fa40a4c
Pavle Jonoski <pavle.jonoski@keitaro.com>
2020-04-09 20:04:44 +0200

    Adds the /news route to the new news feed page.

---
+++ ckanext/knowledgehub/views/newsfeed.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+import logging
+
+from flask import Blueprint
+
+import ckan.lib.base as base
+import ckan.lib.helpers as h
+import ckan.lib.navl.dictization_functions as dict_fns
+import ckan.logic as logic
+import ckan.model as model
+
+
+newsfeed = Blueprint(
+    u'news',
+    __name__,
+    url_prefix=u'/news'
+)
+
+
+def _get_context():
+    return dict(model=model, user=g.user,
+                auth_user_obj=g.userobj,
+                session=model.Session)
+
+
+def news():
+    u'''List all news on the feed.
+    '''
+
+    extra_vars = {}
+
+    extra_vars["page"] = h.Page(
+        collection=[],
+        page=1,
+        url=h.pager_url,
+        items_per_page=20)
+
+    extra_vars['page'].items = []
+
+    return base.render(u'news/index.html',
+                       extra_vars=extra_vars)
+
+
+newsfeed.add_url_rule(u'/', view_func=news, strict_slashes=False)
