f3263d5b9a89fe19e0b91a7378c489a7778e6e64
Pavle Jonoski <pavle.jonoski@keitaro.com>
2020-05-06 09:30:38 +0200

    Implements the Request Access result page.

--- ckanext/knowledgehub/views/access.py
+++ ckanext/knowledgehub/views/access.py
@@ -12,6 +12,8 @@ from ckan.logic import (
     get_action,
     check_access,
     NotAuthorized,
+    NotFound,
+    ValidationError,
 )
 
 
@@ -37,10 +39,35 @@ def request(entity_type, entity_ref):
         check_access('request_access', context, {})
     except NotAuthorized:
         base.abort(403, _('You do not have pemrission to request access '
-                          'on this resource.'))
+                          'for this resource.'))
         return
 
-    return base.render('access/request.html', extra_vars={})
+    extra_vars = {}
+    errors = []
+    try:
+        access_request = get_action('request_access')(context, {
+            'entity_type': entity_type,
+            'entity_ref': entity_ref,
+        })
+        extra_vars['access_request'] = access_request
+    except NotAuthorized:
+        errors.append(_('You do not have permission to request access for '
+                        'this resource.'))
+    except NotFound:
+        errors.append(_('The referenced resource was not found.'))
+    except ValidationError as e:
+        for key, value in e.error_summary.items():
+            errors.append('{} - {}'.format(key, value))
+    except Exception as e:
+        log.error('Failed to execute request_access. Error: %s', str(e))
+        log.exception(e)
+        base.abort(500, _('An unexpected error occured while processing your '
+                          'request. Error: %s' % str(e)))
+        return
+
+    extra_vars['errors'] = errors
+
+    return base.render('access/request.html', extra_vars=extra_vars)
 
 
 access.add_url_rule('/request/<entity_type>/<entity_ref>', view_func=request)
