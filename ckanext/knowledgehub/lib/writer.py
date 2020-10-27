dbe85bc2e68c4031dc33749497234208a88df946
Petar Efnushev <petar.efnushev@gmail.com>
2020-10-27 13:59:55 +0100

    Clean-up csv writer

--- ckanext/knowledgehub/lib/writer.py
+++ ckanext/knowledgehub/lib/writer.py
@@ -6,33 +6,30 @@ import cStringIO
 log = logging.getLogger(__name__)
 
 
-class WriterService():
-
+class WriterService:
     def csv_writer(self, fields, records, delimiter):
 
-        d = str(delimiter).lower()
-
-        columns = [str(x['id']).encode("utf-8") for x in fields]
+        columns = [str(f['id']).encode("utf-8") for f in fields]
         output = cStringIO.StringIO()
 
-        if d == 'semicolon':
+        delimiter = str(delimiter).lower()
+        if delimiter == 'semicolon':
             writer = csv.writer(output, delimiter=';')
-        elif d == 'pipe':
+        elif delimiter == 'pipe':
             writer = csv.writer(output, delimiter='|')
-        elif d == 'tab':
+        elif delimiter == 'tab':
             writer = csv.writer(output, dialect='excel-tab')
         else:
             writer = csv.writer(output, delimiter=',')
 
         # Writing headers
-        writer.writerow([str(f['id']).encode("utf-8") for f in fields])
+        writer.writerow(columns)
 
         # Writing records
         for record in records:
             writer.writerow([record[column] for column in columns])
 
         file_content = cStringIO.StringIO(output.getvalue())
-
         output.close()
 
         return file_content
